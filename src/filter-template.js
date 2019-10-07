8afb7209a8a75335c15c650e915747b4151d8e4b
Andrii <andrii.hodniuk@outlook.com>
2019-10-07 13:12:44 +0200

    Added function to generate filter item by template

---
+++ src/filter-template.js
@@ -0,0 +1,17 @@
+import {ucfirst} from './helpers';
+
+const filterTemplate = (data) => {
+  return `
+    <a 
+    href="#${data.label}" 
+    class="main-navigation__item 
+    ${data.label === `stats` ? `main-navigation__item--additional` : ``} 
+    ${data.label === `all` ? `main-navigation__item--active` : ``}"
+    >
+    ${ucfirst(data.label)} 
+    ${data.number > 0 ? `<span class="main-navigation__item-count">${data.number}</span>` : ``}
+    </a>
+  `;
+};
+
+export default filterTemplate;
